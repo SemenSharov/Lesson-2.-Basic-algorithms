e85d5fce319bfc1681f005349b91fdebd68f1e1e
SemenSharov <xxxSharik@gmail.com>
2023-03-11 18:32:13 +0300

    Solved task 10

--- Program.cs
+++ Program.cs
@@ -5,20 +5,6 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int a = Convert.ToInt32(100);
-int b = Convert.ToInt32(1000);
-if ((a <= number) && (number < b))
-{
-    int number1 = number % 100;
-    int number2 = number1 / 10;
-    Console.WriteLine(number2);
-}
-else
-{
-    Console.WriteLine("Вводить только трехзначное число");
-}
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 // 645 -> 5
@@ -26,6 +12,42 @@ else
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 99;
+int b = 999;
+int c = 9999;
+int d = 99999;
+
+if ((a < number) && (b > number))
+{
+    int number1 = number % 100;
+    int number2 = number1 % 10;
+    Console.WriteLine(number2);
+}
+if ((b < number) && (c > number))
+{
+    int number1 = number % 1000;
+    int number2 = number1 % 100;
+    int number3 = number2 % 10;
+    Console.WriteLine(number3);
+}
+
+if ((c < number) && (d > number))
+{
+    int number1 = number / 100 % 10;
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.WriteLine("Слишком большое число");
+}
+
+if (number < a)
+{
+    Console.WriteLine("Нет третьего числа");
+}
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
