e4e138105427916b7d735511e0923c93686bb193
SemenSharov <xxxSharik@gmail.com>
2023-03-15 18:52:15 +0300

    did the task15

--- Program.cs
+++ Program.cs
@@ -18,3 +18,20 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Задача 15 ");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("(этот день выходной) -> да");
+}
+else if (dayNumber < 1 || dayNumber > 7)
+{
+    Console.WriteLine("это вообще не день недели");
+}
+else Console.WriteLine("(этот день не выходной) -> нет");
